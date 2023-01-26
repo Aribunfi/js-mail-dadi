093eb08a97e8a5294fb32e70475c614416552270
Aribunfi <arianna@pabis.de>
2023-01-26 17:33:31 +0100

    tirano i dadi

--- gioco-dadi/main.js
+++ gioco-dadi/main.js
@@ -1,21 +1,46 @@
 // Gioco dei dadi Generare un numero random da 1 a 6,
 
-const numbers = [1,2,3,4,5,6];
+ const randomNumber = [1,2,3,4,5,6];
 
-const games = Math.floor(Math.random()*6) + 1;
-numbers.push(randomNumber);
+    const games = Math.floor(Math.random()*6) + 1;
+    randomNumber.push(randomNumber);
 
 console.log('numbers');
 
-// sia per il giocatore sia per il computer.
+// // // sia per il giocatore sia per il computer.
 
 var element = document.getElementById("button");
 
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// // // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
-for (let i = 0; i < numbers.lenght; i++ ) {
+for (let i = 0; i < randomNumber.lenght; i++ ) {
 
-    if (games.player (i)) and (games.pc-player(i))
-
+if (games.player (i)) and (games.pc-player(i))
 }
+
+
+// var dice = {
+//     sides: 6,
+//     roll: function () {
+//         var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//         return randomNumber;
+//     }
+// }
+
+
+
+// //Prints dice roll to the page
+
+// function printNumber(number) {
+//     var placeholder = document.getElementById('placeholder');
+//     placeholder.innerHTML = number;
+// }
+
+// var button = document.getElementById('button');
+
+// button.onclick = function () {
+//     var result = dice.roll();
+//     printNumber(result);
+// };
+
