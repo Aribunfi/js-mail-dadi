1320e1088cdb6a9f3e6cdf97983d11b6a33a50c6
Aribunfi <arianna@pabis.de>
2023-01-26 17:07:10 +0100

    tentativo bottoni

--- gioco-dadi/main.js
+++ gioco-dadi/main.js
@@ -5,15 +5,17 @@ const numbers = [1,2,3,4,5,6];
 const games = Math.floor(Math.random()*6) + 1;
 numbers.push(randomNumber);
 
+console.log(numbers);
+
 // sia per il giocatore sia per il computer.
 
-var element = document.getElementById("player");
+var element = document.getElementById("button");
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
 for (let i = 0; i < numbers.lenght; i++ ) {
 
-    if games.player (i)> games.pc-player(i)
+    if (games.player (i)) and (games.pc-player(i))
 
 }
